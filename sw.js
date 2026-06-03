ae9ea7470d0d8db7eaa49fc9c054081d0df696dd
今橋伸悟 <imahashishingo@imahashieminoMacBook-Air.local>
2026-06-03 11:40:17 +0900

    SW v8: キャッシュバスト

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'idol-bunny-v7';
+const CACHE = 'idol-bunny-v8';
 const FILES = ['/', '/game.html', '/index.html', '/icon-192.png', '/icon-512.png'];
 
 self.addEventListener('install', e => {
